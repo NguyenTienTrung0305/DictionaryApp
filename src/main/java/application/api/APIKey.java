287ac85b71a8c7f63274a2349712cdfef03774de
latuan1 <125745639+latuan1@users.noreply.github.com>
2023-12-04 22:50:00 +0700

    update(not done)

---
+++ src/main/java/application/api/APIKey.java
@@ -0,0 +1,9 @@
+package application.api;
+
+public interface APIKey {
+    String translateURI = "https://microsoft-translator-text.p.rapidapi.com/translate?";
+    String translateKey = "9601f27946mshcf6b2f0b327089fp160fe7jsnb72eca437e5e";
+    String translateHost = "microsoft-translator-text.p.rapidapi.com";
+    String speechKey = "81baf70c342f475291fed4dcdb2d9c0c";
+    String speechRegion = "southeastasia";
+}
---
+++ src/main/java/application/api/TranslateAPI.java
@@ -0,0 +1,37 @@
+package application.api;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class TranslateAPI implements APIKey {
+    public String translate(String input, String from, String to) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(translateURI + "to=" + to + "&api-version=3.0&"
+                        + "from=" + from + "&profanityAction=NoAction&textType=plain"))
+                .header("content-type", "application/json")
+                .header("X-RapidAPI-Key", translateKey)
+                .header("X-RapidAPI-Host", translateHost)
+                .method("POST", HttpRequest.BodyPublishers.ofString("[\r {\r\"Text\": \"" + input + "\"\r}\r]"))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        String cur = response.body();
+        String result = "";
+        int index = 0;
+        for (int i = 0; i < cur.length(); i++) {
+            if (index == 5) {
+                if (cur.charAt(i) != '"') {
+                    result = result + cur.charAt(i);
+                } else {
+                    break;
+                }
+            }
+            if (cur.charAt(i) == '"') {
+                index++;
+            }
+        }
+        return result;
+    }
+}
